8233b8109062a58003b31fae4612266e90b15ed7
Abhishek <3003abhishek.jha@gmail.com>
2024-01-07 21:13:01 +0530

    perform the post operation

--- RestruaApi7/index.js
+++ RestruaApi7/index.js
@@ -2,17 +2,37 @@ const express = require("express");
 const fs = require("fs") ;
 
 const app = express();
+app.use(express.json());
 
-let restuarants = JSON.parse(fs.readFileSync(`${__dirname}/data/restrauData.json`));
+let data = JSON.parse(fs.readFileSync(`${__dirname}/data/restrauData.json`));
+// console.log(data);
 
 app.get("/api/v1/restuarants" , (req,res) =>{
     res.status(200).json({
         status : 200,
-        results : restuarants.length,
-        data : restuarants
+        results : data.restaurants.length,
+        data 
         
     })
-})
+});
+
+
+app.post("/api/v1/restuarants" , (req,res) => {
+
+    let new_restuarant = req.body;
+    console.log(new_restuarant);
+    data.restaurants.push(new_restuarant);
+    console.log(data);
+     fs.writeFile(`${__dirname}/data/restrauData.json` , JSON.stringify(data) , err =>{
+        res.status(201).json({
+            status: 201 ,
+            data : new_restuarant
+        })
+     })
+    });
+
+
+    
 
 
 app.listen(8080, ()=>{

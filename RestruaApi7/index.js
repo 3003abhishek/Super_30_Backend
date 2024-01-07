372862b56dd5abf0dd7f4914b3a00a2143847a7c
Abhishek <3003abhishek.jha@gmail.com>
2024-01-07 22:10:32 +0530

    respond to the url parameters

--- RestruaApi7/index.js
+++ RestruaApi7/index.js
@@ -16,6 +16,59 @@ app.get("/api/v1/restuarants" , (req,res) =>{
     })
 });
 
+app.get(`/api/v1/restuarants/:id`, (req,res) =>{
+    console.log(req.params);
+    let id = Number(req.params.id);
+    let restuarant = data.restaurants.find((el) => el.id == id );
+    if(restuarant){
+        res.status(200).json({
+            status : 200,
+            restuarant : restuarant
+        })
+    }
+    else{
+        res.status(404).json({
+            status : 404,
+            message : "Not found"
+        });
+    }
+    
+});
+
+
+app.get(`/api/v1/restuarants/:id/:foodid`, (req,res) =>{
+    console.log(req.params);
+    let id = Number(req.params.id);
+    let foodId = Number(req.params.foodid);
+
+    let restuarant = data.restaurants.find((el) => el.id == id );
+    if(restuarant){
+         
+        let food = restuarant.foods.find(el => el.id == foodId);
+        if(food){
+            res.status(200).json({
+                status : 200,
+                food
+            })
+        }
+        else{
+            res.status(404).json({
+                status : 404,
+                message : "Not found"
+            });
+        }
+    }
+    else{
+        res.status(404).json({
+            status : 404,
+            message : "Not found"
+        });
+    }
+    
+});
+
+
+
 
 app.post("/api/v1/restuarants" , (req,res) => {
 
